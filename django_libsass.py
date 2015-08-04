8b9186ca3a959867d87dc5b9c11042f3d94062b2
apocquet <apocquet@lecko.fr>
2015-08-04 13:46:55 +0200

    Allow custom function injection
    Add a LIBSASS_CUSTOM_FUNCTIONS parameter to the project settings.
    This dictionary maps function names to callables that should be added to your Sass compiler.
    
    By default, a 'static' function is added to the Sass environment, which mimics the 'static' template tag.

--- django_libsass.py
+++ django_libsass.py
@@ -1,11 +1,23 @@
 from django.conf import settings
 from django.contrib.staticfiles.finders import get_finders
+from django.templatetags.static import static as django_static
 
 import sass
 from compressor.filters.base import FilterBase
 
+
+def static(path):
+    """
+    Use the Django builtin static file resolver to return an absolute path
+    usable as CSS url() argument. Sass equivalent of the 'static' template
+    tag.
+    """
+    return '"{}"'.format(django_static(path))
+
+
 OUTPUT_STYLE = getattr(settings, 'LIBSASS_OUTPUT_STYLE', 'nested')
 SOURCE_COMMENTS = getattr(settings, 'LIBSASS_SOURCE_COMMENTS', settings.DEBUG)
+CUSTOM_FUNCTIONS = getattr(settings, 'LIBSASS_CUSTOM_FUNCTIONS', {'static': static})
 
 
 def get_include_paths():
@@ -42,7 +54,12 @@ def compile(**kwargs):
         INCLUDE_PATHS = get_include_paths()
 
     kwargs = kwargs.copy()
-    kwargs['include_paths'] = (kwargs.get('include_paths') or []) + INCLUDE_PATHS
+    kwargs['include_paths'] = kwargs.get('include_paths', []) + INCLUDE_PATHS
+
+    custom_functions = CUSTOM_FUNCTIONS.copy()
+    custom_functions.update(kwargs.get('custom_functions', {}))
+    kwargs['custom_functions'] = custom_functions
+
     return sass.compile(**kwargs)
 
 
@@ -59,4 +76,3 @@ class SassCompiler(FilterBase):
         else:
             return compile(string=self.content,
                            output_style=OUTPUT_STYLE)
-
