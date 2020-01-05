bbed4de8bd2f3ae6c65b0adc3cf371650d09d934
Matt Westcott <matt@west.co.tt>
2020-01-05 18:53:32 +0000

    Add test case for scss file with import

--- tests/tests/test_sass.py
+++ tests/tests/test_sass.py
@@ -1,7 +1,14 @@
+import os.path
+from django.conf import settings
 from django.test import TestCase
 
+from django_libsass import compile
 
 class TestSass(TestCase):
-    def test_sass(self):
+    def test_invocation(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_import(self):
+        result = compile(filename=os.path.join(settings.BASE_DIR, 'tests', 'static', 'css', 'with_import.scss'))
+        self.assertIn('.imported-style', result)
